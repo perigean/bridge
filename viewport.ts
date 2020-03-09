53d98b9f29806a61929132c5f7f787a61f008c52
Charles <cwdick@gmail.com>
2020-03-09 22:09:13 +0900

    some more startup stuff

--- viewport.ts
+++ viewport.ts
@@ -5,7 +5,7 @@ import {Affine2D, Point2D, transformTranslateCreate, transformStretch, transform
 
 // Viewport keeps track of world <-> screen coordinates
 // World coordinates are +x -> right, +y -> up
-class Viewport {
+export class Viewport {
     private ctx: CanvasRenderingContext2D;
     private pos: Point2D;
     private scale: number;
@@ -15,23 +15,22 @@ class Viewport {
     w2s: Affine2D;
     s2w: Affine2D;
 
-    private resetTransforms() {
+    private computeWorldToScreen(): Affine2D {
         const canvas = this.ctx.canvas;
         const width = canvas.width;
         const height = canvas.height;
         let s2w = transformTranslateCreate(-width * 0.5, -height * 0.5);
         s2w = transformStretch(s2w, this.scale, -this.scale);
         s2w = transformTranslate(s2w, -this.pos[0], -this.pos[1]);
-        this.s2w = s2w;
-        this.w2s = transformInvert(s2w);
-        this.ctx.transform(s2w[0], s2w[3], s2w[1], s2w[4], s2w[2], s2w[5]);
+        return s2w;
     }
 
     constructor(ctx: CanvasRenderingContext2D, pos: Point2D, scale: number) {
         this.ctx = ctx;
         this.pos = pos;
         this.scale = scale;
-        this.resetTransforms();
+        this.w2s = this.computeWorldToScreen();
+        this.s2w = transformInvert(this.w2s);
     }
 
 }
