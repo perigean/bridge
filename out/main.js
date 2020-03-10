dbfdee2413afb7d91ea69868a7bc50d6f97166fe
Charles <cwdick@gmail.com>
2020-03-10 21:20:56 +0900

    hook script up to index

---
+++ out/main.js
@@ -0,0 +1,6 @@
+"use strict";
+function main() {
+    console.log("Hello, World!");
+}
+main();
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uL3NyYy9tYWluLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQSxTQUFTLElBQUk7SUFDVCxPQUFPLENBQUMsR0FBRyxDQUFDLGVBQWUsQ0FBQyxDQUFDO0FBQ2pDLENBQUM7QUFFRCxJQUFJLEVBQUUsQ0FBQyIsInNvdXJjZXNDb250ZW50IjpbImZ1bmN0aW9uIG1haW4oKSB7XG4gICAgY29uc29sZS5sb2coXCJIZWxsbywgV29ybGQhXCIpO1xufVxuXG5tYWluKCk7XG4iXX0=
