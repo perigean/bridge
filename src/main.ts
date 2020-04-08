b62558dd6a63698563be9281d2e956ee2321fcd2
Charles <cwdick@gmail.com>
2020-04-08 21:08:14 +0900

    log touch distances
    to see what the tap move threshold should be

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { Viewport, ViewportPosition } from "./viewport.js"
-//import { Point2D } from "./transform.js"
+import { Point2D } from "./transform.js"
 
 const c = (document.getElementById("canvas") as HTMLCanvasElement);
 if (c === null) {
@@ -34,34 +34,50 @@ export class ViewportTouch {
     }
 };
 
+const points = new Map<number, {start: Point2D, move: Point2D}>();
+
 function touchStart(evt: TouchEvent) {
     evt.preventDefault();
     console.log("touchstart");
     for (const t of evt.touches) {
-        console.log("    " + t.identifier + ": " + t.clientX + ", " + t.clientY);
+        if (!points.has(t.identifier)) {
+            points.set(t.identifier, {start: [t.clientX, t.clientY], move: [t.clientX, t.clientY]});
+        }
     }
 }
 
-function touchEndGenerator(name: string) {
-    return function touchEnd(evt: TouchEvent) {
-        evt.preventDefault();
-        console.log(name);
-        for (const t of evt.touches) {
-            console.log("    " + t.identifier + ": " + t.clientX + ", " + t.clientY);
+function touchEnd(evt: TouchEvent) {
+    evt.preventDefault();
+    
+    const removed = new Set<number>(points.keys());
+    for (const t of evt.touches) {
+        removed.delete(t.identifier);
+    }
+    for (const i of removed) {
+        const t = points.get(i);
+        if (!t) {
+            throw new Error("missing point with id " + i);
         }
-    };
-}
+        const d = [t.move[0] - t.start[0], t.move[1] - t.start[1]];
+        console.log("touch " + i + " moved " + Math.sqrt(d[0] * d[0] + d[1] * d[1]));
+        points.delete(i);
+    }
+};
 
 function touchMove(evt: TouchEvent) {
     evt.preventDefault();
-    console.log("touchmove");
     for (const t of evt.touches) {
-        console.log("    " + t.identifier + ": " + t.clientX + ", " + t.clientY);
+        const p = points.get(t.identifier);
+        if (!p) {
+            throw new Error("missing point with id " + t.identifier);
+        }
+        p.move[0] = t.clientX;
+        p.move[1] = t.clientY;
     }
 }
-    
+
 c.addEventListener("touchstart", touchStart, false);
-c.addEventListener("touchend", touchEndGenerator("touchend"), false);
-c.addEventListener("touchcancel", touchEndGenerator("touchcancel"), false);
+c.addEventListener("touchend", touchEnd, false);
+c.addEventListener("touchcancel", touchEnd, false);
 c.addEventListener("touchmove", touchMove, false);
 console.log("stuff loaded");
